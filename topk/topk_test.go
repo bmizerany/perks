1493622576a3bac35a49669a7feb9161b1490221
Ben Linsay <blinsay@gmail.com>
2015-05-08 02:10:43 -0400

    add a test for the degenerate topk case
    - the case occurs when an element that was the minimum grows to become
      not the minimum anymore and then another element is added.

--- topk/topk_test.go
+++ topk/topk_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"math/rand"
 	"sort"
+	"strconv"
 	"testing"
 )
 
@@ -37,9 +38,57 @@ func TestTopK(t *testing.T) {
 	}
 }
 
+func TestTopElements(t *testing.T) {
+	N := 4
+
+	// A stream keeps N+1 items internally, so prime it with N+1 items. Use the
+	// integers 1 to N+1 and give int n frequency n.
+	stream := New(N)
+	for i := 1; i <= N+1; i++ {
+		for j := 0; j < i; j++ {
+			stream.Insert(fmt.Sprintf("%d", i))
+		}
+	}
+
+	// Make sure the insertion went ok.
+	for _, sample := range stream.Query() {
+		actual, err := strconv.Atoi(sample.Value)
+		if err != nil {
+			panic(err)
+		}
+		if actual != sample.Count {
+			t.Fatalf("expected element %s to have value %d: got %d", sample.Value, actual, sample.Count)
+		}
+	}
+
+	// Insert 1 twice more so that "2" is the smallest element with a count of 2,
+	// and then insert N * 2 four times. "2" should drop out of the top k, and
+	// N * 2 should appear in the sample.
+	newElement := fmt.Sprintf("%d", N*2)
+	stream.Insert("1")
+	stream.Insert("1")
+	stream.Insert(newElement)
+	stream.Insert(newElement)
+	stream.Insert(newElement)
+	stream.Insert(newElement)
+
+	var sawNewElement bool
+	for _, sample := range stream.Query() {
+		if sample.Value == "2" {
+			t.Fatalf("expected 2 to drop")
+		}
+		if sample.Value == newElement {
+			sawNewElement = true
+		}
+	}
+	if !sawNewElement {
+		t.Fatalf("didn't see newly inserted element in the topk")
+	}
+}
+
 func TestQuery(t *testing.T) {
 	queryTests := []struct {
-		value string
+		value    string
 		expected int
 	}{
 		{"a", 1},
